9647f9c1f040e255c02d7184e9e1687d6e9ab410
ankitav456 <ankitav456@gmail.com>
2024-09-27 17:56:35 +0530

    added font family in body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,10 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Ankita Vishwakarma - Web Developer Portfolio",
-  description: "Explore Ankita Vishwakarma's projects, skills, and experience in web development, specializing in React and Next.js.",
+  description:
+    "Explore Ankita Vishwakarma's projects, skills, and experience in web development, specializing in React and Next.js.",
 };
-
 
 export default function RootLayout({
   children,
@@ -26,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
