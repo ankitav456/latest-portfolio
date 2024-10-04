2c97fca49b803db82f53bbe4939b582fea158e93
ankitav456 <ankitav456@gmail.com>
2024-10-04 09:23:23 +0530

    structured layout of avatar

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,55 +1,43 @@
 import { FunctionComponent } from "react";
 import "./style.css";
-import avatar from "@/components/images/avatar.svg";
+import avatar from "../images/image-avatar.png"; // Use correct path for image import
+import Image from "next/image";
 
 interface HomeSectionProps {}
 
 const HomeSection: FunctionComponent<HomeSectionProps> = () => {
   return (
     <div className="home-main-container custom-theme">
-      {/* <div className="avatar-section">
-        <img src="avatar-girl.png" alt="Avatar" className="avatar" />
-        <h1>
-          Hello! I am <span>Ankita Vishwakarma</span>
-        </h1>
-        <p className="description">
-          A designer who judges a book by its cover...
-          <br />
-          Because if the cover does not impress you, what else can?
-        </p>
-      </div>
-      <div className="main-content">
-        <h2>
-          I'm a <span className="highlight">Software Engineer</span>.
-        </h2>
-        <p className="job-details">
-          Currently, I'm a Software Engineer at <span>Facebook</span>.
-        </p>
-        <p className="about">
-          A self-taught UI/UX designer, functioning in the industry for 3+ years
-          now. I make meaningful and delightful digital products that create an
-          equilibrium between user needs and business goals.
-        </p>
-      </div> */}
-
       <div className="home-main-section">
         <div className="home-avatar-section">
-          <img src="images/image-avatar.png" alt="Avatar" className="avatar" />
-        <div className="home-avatar-content-section"></div>
+          <Image
+            src={avatar}
+            alt="Avatar"
+            className="avatar"
+            width={260}
+            height={260}
+          />
         </div>
-        <div className="main-content">
-        <h2>
-          I'm a <span className="highlight">Software Engineer</span>.
-        </h2>
-        <p className="job-details">
-          Currently, I'm a Software Engineer at <span>Facebook</span>.
-        </p>
-        <p className="about">
-          A self-taught UI/UX designer, functioning in the industry for 3+ years
-          now. I make meaningful and delightful digital products that create an
-          equilibrium between user needs and business goals.
-        </p>
-      </div>
+        <div className="home-content-section">
+          <div className="description">
+            <span>Hello! I am Ankita Vishwakarma</span>
+            <h1>A designer who judges a book by its cover...</h1>
+            <p>Because if the cover does not impress you, what else can?</p>
+          </div>
+          <div className="main-content">
+            <h2>
+              I'm a <span className="highlight">Software Engineer</span>.
+            </h2>
+            <p className="job-details">
+              Currently, I'm a Software Engineer at <span>Facebook</span>.
+            </p>
+            <p className="about">
+              A self-taught UI/UX designer, functioning in the industry for 3+
+              years now. I make meaningful and delightful digital products that
+              create an equilibrium between user needs and business goals.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
